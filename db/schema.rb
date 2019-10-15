14624bcafb6764802ca253e9620195e114f2a203
EC2 Default User <ec2-user@ip-172-31-44-4.ec2.internal>
2019-10-15 07:33:39 +0000

    Scaffolding de user

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_073000) do
+ActiveRecord::Schema.define(version: 2019_10_15_073242) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "description"
     t.boolean "sttaus"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "firstname"
+    t.string "lastname"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
